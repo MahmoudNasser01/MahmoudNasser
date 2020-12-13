93e0cc60f8f290317526bcdb8b89599739447d86
elMeniwy <vego2000im@gmail.com>
2020-12-13 20:18:53 +0200

    first Commit

---
+++ js.js
@@ -0,0 +1,11 @@
+const homeLink = document.getElementById('home-link');
+
+const headerView = document.getElementById('header')
+
+homeLink.addEventListener('click' , ()=>{
+    console.log('Clicked')
+        headerView.style.transform
+
+
+
+})
